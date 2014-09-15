657f8284d0844ad307aaa4a898628b25cf046bf4
lemon <limengabc@163.com>
2014-09-15 16:46:02 +0800

    update for roboguice

--- src/main/java/com/lemon/android/HelloAndroidActivity.java
+++ src/main/java/com/lemon/android/HelloAndroidActivity.java
@@ -3,17 +3,21 @@ package com.lemon.android;
 import android.os.Bundle;
 import android.widget.TextView;
 import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectResource;
 import roboguice.inject.InjectView;
 
 public class HelloAndroidActivity extends RoboActivity {
     @InjectView(R.id.textView)
     TextView textView;
 
+    @InjectResource(R.string.hello_world)
+    String text;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView.setText("Hello!");
+        textView.setText(text);
     }
 
 }
